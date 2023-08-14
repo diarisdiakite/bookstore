3a0c60aea5baae7e3be575f8043bdab0b8363f04
diarisdiakite <diarisdiakite@gmail.com>
2023-08-14 05:05:36 +0200

    Add pages components.

---
+++ src/components/pages/CategoriesPages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CategoriesPages() {
+  return (
+    <div className="main-container">
+      CategoriesPages
+    </div>
+  );
+}
+
+export default CategoriesPages;
