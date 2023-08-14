c32a8ec53db789073721036ffd9df713e530a437
diarisdiakite <diarisdiakite@gmail.com>
2023-08-15 01:34:24 +0200

    Added storage and an author page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
@@ -10,14 +10,25 @@ import AddNewBookForm from './components/pages/AddNewBookForm';
 import NotFoundPage from './components/pages/NotFoundPage';
 
 function App() {
+  const books = useState(JSON.parse(localStorage.getItem('books')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books));
+  }, [books]);
+
+  const HandleDelete = (id) => {
+    const listBooks = books.filter((book) => book.id !== id);
+    localStorage.setItem(listBooks, JSON.stringify(books));
+  };
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/books">
-          <Route index element={<BooksListPage />} />
-          <Route path=":id" element={<BookPage />} />
+          <Route index element={<BooksListPage HandleDelete={HandleDelete} />} />
+          <Route path=":id" element={<BookPage HandleDelete={HandleDelete} />} />
           <Route path="new" element={<AddNewBookForm />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
