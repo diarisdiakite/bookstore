d00b6bb3bc6dfe078b54ec8eb4d747b929bb610c
diarisdiakite <diarisdiakite@gmail.com>
2023-08-15 01:37:14 +0200

    Added an individual book state, as well as a button to delete the book. Run linters.

--- src/components/pages/BooksListPage.js
+++ src/components/pages/BooksListPage.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React
+  from 'react';
+import { Link } from 'react-router-dom';
 
 function BooksListPage() {
   return (
     <div className="main-container">
-      BooksListPage
+      <h1>BooksListPage</h1>
+      <Link to="/books/1">Book 1</Link>
     </div>
   );
 }
