41229c0f7a3e8d44b69e6765c599f6f1910f101d
diarisdiakite <diarisdiakite@gmail.com>
2023-08-15 03:09:33 +0200

    Add form elements labels, inputs and a button to add a new book.

--- src/components/pages/BooksListPage.js
+++ src/components/pages/BooksListPage.js
@@ -6,7 +6,11 @@ function BooksListPage() {
   return (
     <div className="main-container">
       <h1>BooksListPage</h1>
+      <Link to="/books/new">Add a new Book</Link>
+      <h2>Your saved books</h2>
       <Link to="/books/1">Book 1</Link>
+      <Link to="/books/2">Book 2</Link>
+      <Link to="/books/3">Book 3</Link>
     </div>
   );
 }
