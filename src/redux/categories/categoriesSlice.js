04a96c68dd2b42378485389271085731b215ec72
diarisdiakite <diarisdiakite@gmail.com>
2023-08-21 19:50:10 +0200

    Add the condition that the categories reducer checks the status from the action payload before returning the message.

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -11,32 +11,10 @@ const categoriesSlice = createSlice({
   name: 'category',
   initialState,
   reducers: {
-    addCategory: (state, action) => {
-      state.categories.push(action.payload);
-    },
-    removeCategory: (state, action) => {
-      const idToRemove = action.payload;
-      state.categories = state.categories.filter((category) => category.id !== idToRemove);
-    },
-    updateCategory: (state, action) => {
-      const {
-        id, title, author, category,
-      } = action.payload;
-      const categoryIndex = state.categories.findIndex(
-        (category) => category.id === categoryIndex,
-      );
-      if (categoryIndex !== -1) {
-        state.categories[categoryIndex] = {
-          id, title, author, category,
-        };
-      }
-    },
     checkCategory: (state, action) => {
-      action.payload = 'Under construction';
-      return action.payload;
-      /*
-      state.categories = 'Under construction';
-      return state.categories */
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
     },
   },
 
