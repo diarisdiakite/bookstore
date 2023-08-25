b8e3ffa9123e3e905d989536887de8a80e908e93
diarisdiakite <diarisdiakite@gmail.com>
2023-08-26 00:10:15 +0200

    Add a scss module for and style books components.

--- src/redux/books/childComponents/BooksListElements.js
+++ src/redux/books/childComponents/BooksListElements.js
@@ -1,46 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { deleteBook } from '../booksSlice';
+import classes from '../../../assets/css/books.module.scss';
+import Completed from './Completed';
+import Current from './Current';
 
 function BooksListElements({ books }) {
   const dispatch = useDispatch();
+
+  /* const handleReadingDetailsChange = (bookId, currentChapter, percentageCompleted) => {
+    setReadingDetails((prevDetails) => ({
+      ...prevDetails,
+      [bookId]: {
+        currentChapter,
+        percentageCompleted,
+      },
+    }));
+  }; */
+
+  const [readingDetailsState, setReadingDetailsState] = useState({
+    currentChapter: '',
+    percentageCompleted: 0,
+  });
+
+  const setReadingDetails = (bookId) => {
+    if (bookId === Object.entries(books)[0].id) {
+      setReadingDetailsState({
+        currentChapter: 'Chapter 17',
+        percentageCompleted: 64,
+      });
+    } else if (bookId === Object.entries(books)[1].id) {
+      setReadingDetailsState({
+        currentChapter: 'Chapter 13: "A lesson learned"',
+        percentageCompleted: 8,
+      });
+    } else {
+      setReadingDetailsState({
+        currentChapter: 'Introduction',
+        percentageCompleted: 0,
+      });
+    }
+  };
 
   return (
     <div className="main-container">
       {Object.entries(books).map(([bookId, bookArray]) => {
         const bookData = bookArray[0];
         return (
-          <div key={bookId}>
-            <div className="book-card">
-              <div className="book">
-                <div className="text">
-                  <p key={`category-${bookId}`}>{bookData.category}</p>
-                  <h2><Link to={`/books/${bookId}`}>{bookData.title}</Link></h2>
-                  <p key={`author-${bookId}`}>{bookData.author}</p>
-                </div>
-                <div className="buttons">
-                  <Link to="/*" className="button-white">Comment</Link>
-                  <button
-                    type="button"
-                    className="button-white"
-                    aria-label="Update book"
-                  >
-                    | Update the book |
-                  </button>
-                  <button
-                    type="button"
-                    className="button-white"
-                    aria-label="Remove book"
-                    onClick={() => dispatch(deleteBook(bookId))}
-                  >
-                    Remove the book
-                  </button>
-                </div>
+          <div key={bookId} className={classes.books__bookCard}>
+            <div className={classes.books__bookCard__book}>
+              <div className={classes.books__bookCard__book__text}>
+                <p key={`category-${bookId}`}>{bookData.category}</p>
+                <h2><Link to={`/books/${bookId}`}>{bookData.title}</Link></h2>
+                <p key={`author-${bookId}`} className={classes.blue}>{bookData.author}</p>
               </div>
-              <div className="progress" />
-              <div className="current-chapter" />
+              <div className={classes.books__bookCard__book__links}>
+                <Link to="/*" className={classes.books__bookCard__book__button__comments}>Comment</Link>
+                <button
+                  type="button"
+                  className={classes.books__bookCard__book__button__remove}
+                  aria-label="Remove book"
+                  onClick={() => dispatch(deleteBook(bookId))}
+                >
+                  | Remove |
+                </button>
+                <button
+                  type="button"
+                  className={classes.books__bookCard__book__button__edit}
+                  aria-label="Update book"
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+            <div className={classes.books__bookCard__book__completed}>
+              <Completed
+                bookId={bookId}
+                readingDetails={readingDetailsState}
+                setReadingDetails={() => setReadingDetails(bookId)}
+              />
+            </div>
+            <div className={classes.books__bookCard__book__currentChapter}>
+              <Current
+                bookId={bookId}
+                readingDetails={readingDetailsState}
+                setReadingDetails={() => setReadingDetails(bookId)}
+              />
             </div>
           </div>
         );
---
+++ src/redux/books/childComponents/Completed.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import PropTypes from 'prop-types';
+import classes from '../../../assets/css/books.module.scss';
+
+const Completed = ({ readingDetails }) => (
+  <div className={classes.books__bookCard__completed}>
+    <div>
+      <CircularProgressbar
+        value={readingDetails.percentageCompleted}
+        className={classes.books__bookCard__completed__imagePercentage}
+      />
+    </div>
+    <div className="">
+      <h3 className={classes.books__bookCard__completed__textPercentage}>
+        {readingDetails.percentageCompleted}
+        %
+      </h3>
+      <span className={classes.books__bookCard__completed__textCompleted}>Completed</span>
+    </div>
+  </div>
+);
+
+Completed.propTypes = {
+  readingDetails: PropTypes.shape({
+    currentChapter: PropTypes.string.isRequired,
+    percentageCompleted: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Completed;
