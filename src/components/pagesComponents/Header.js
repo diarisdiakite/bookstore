246e63cc1e33b14422cc439c75b55a9bf2d6b100
diarisdiakite <diarisdiakite@gmail.com>
2023-08-26 04:33:52 +0200

    Remove "Home" navLink. Remove counters from the header. Resize css elements.

--- src/components/pagesComponents/Header.js
+++ src/components/pagesComponents/Header.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { BsPersonCircle } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
-import { selectAllBooksIds } from '../../redux/books/booksSlice';
-import { selectAllCategories } from '../../redux/categories/categoriesSlice';
 import classes from '../../assets/css/header.module.scss';
 
 function Header() {
-  const books = useSelector(selectAllBooksIds);
-  const booksLength = books.length;
-
-  const categories = useSelector(selectAllCategories);
-  const categoriesLength = categories.length || 0;
-
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
@@ -22,17 +13,11 @@ function Header() {
           <span>CMS</span>
         </h1>
         <nav className={classes.header__content__nav}>
-          <NavLink to="/" className={classes.header__content__nav.navLink}>Home</NavLink>
           <NavLink to="/books" className={classes.header__content__nav.navLink}>
-            Books
-            (
-            <span>{booksLength}</span>
-            )
+            BOOKS
           </NavLink>
           <NavLink to="/categories" className={classes.NavLink}>
-            Categories (
-            <span>{categoriesLength}</span>
-            )
+            CATEGORIES
           </NavLink>
         </nav>
         <div className={classes.header__content__user}>
